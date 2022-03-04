f2d6c7142c0e6abb94c2dd610948cfc17061856c
julia <julia.andrade.silva2004@gmail.com>
2022-03-04 09:06:33 -0300

    pagina carometro terminar

--- Carometro-Nota10-Web/front/src/Pages/Carometro/carometro.jsx
+++ Carometro-Nota10-Web/front/src/Pages/Carometro/carometro.jsx
@@ -2,11 +2,12 @@ import React, { useEffect } from 'react'
 import axios from 'axios'
 import  { useState } from 'react';
 import "../../assets/css/style.css";
-import logo from '../../assets/img/LogoNota.png'
-
+import logo from '../../assets/img/LogoNota.png';
+import SideBar2 from '../../components/SideBar/SideBar2';
+import PerfilFT from '../../assets/img/Perfil.png';
+import setinha from '../../assets/img/setinha.png';
 
 export default function Carometro() {
-
 
     //States
     const [listaAluno, setListaAlunos] = useState([]);
@@ -81,7 +82,40 @@ export default function Carometro() {
 
     return (
         <div>
-            <div>alunos</div>
+            <header >
+                <div className="container container_header">
+                    <SideBar2/>
+                </div>
+                <div>
+                    <img className="imagem_logo" src={logo} alt="Logo Nota 10" />
+                    <img className="container_foto" src={PerfilFT} alt="Foto Perfil"/>
+                </div>
+            </header>
+            <main>
+                <form className="pesquisa" onSubmit={BuscarAlunos}> 
+                    <input className="input_pesquisa" />
+                </form>
+                
+                <div className='container container_alunos'>
+                    {
+                        <div className='box_aluno'>
+                            <div className='foto_aluno'>
+                                <img />
+                            </div>
+                            <div className='nome_aluno'>
+                                <span>{nomeAluno}</span>
+                            </div>
+                            <div className='situacao_aluno'>
+                                <span> {}</span>
+                            </div>
+                            <div>
+                                <img className="seta" src={setinha} alt="Seta"/>
+                            </div>
+                        </div>
+                    }
+                </div>
+
+            </main>
         </div>
     );
 }
