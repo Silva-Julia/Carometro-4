066aae84609bb673ce677b9f5fbdd11f57a80f0f
Enzzo Jacó <enzzo.jaco@portalsesisp.org.br>
2022-03-04 15:46:52 -0200

    Html e Css Fnalizado da pagina de Perfil

--- Carometro-Nota10-Web/front/src/Pages/Perfil/perfil.js
+++ Carometro-Nota10-Web/front/src/Pages/Perfil/perfil.js
@@ -5,28 +5,36 @@ import axios from "axios";
 
 import Logo from '../../assets/img/LogoPerfil.png';
 import PerfilFT from '../../assets/img/Perfil.png';
+import SideBar2 from "../../components/SideBar/SideBar2";
 
-export default function Perfil(){
-    return(
-        <div className="container_fundo">
+export default function Perfil() {
+
+    
+    return (
+        <div>
+            <SideBar2 />
             <div className="container">
                 <img className="imagem_logo" src={Logo} alt="Logo Nota 10" />
                 <div className="container_foto">
-                    <h1>Seu Perfil</h1>
+                    <h1 className="margin_foto" >Seu Perfil</h1>
                     <div className="contaier_foto_editar">
-                        <img src={PerfilFT} alt="Foto Perfil" />
-                        <button>Editar Foto</button>
-                    
+                        <img className="foto_perfil" src={PerfilFT} alt="Foto Perfil" />
+                        <button className="estilo_editar_button">Editar Foto</button>
+
                     </div>
                 </div>
-                <div>
+                <div className="margin_geral_info">
                     <div>
-                        <h1>Nome : </h1>
-                        <span>Test</span>
+                        <h1 className="margin_info">Nome : </h1>
+                        <div className="container_info">
+                            <span className="margin_left_info">Nome e Sobrenome</span>
+                        </div>
                     </div>
                     <div>
-                        <h1>Email : </h1>
-                        <span>email@email.com</span>
+                        <h1 className="margin_info margin_top_info">Email : </h1>
+                        <div className="container_info">
+                            <span className="margin_left_info">email@email.com</span>
+                        </div>
                     </div>
                 </div>
             </div>
